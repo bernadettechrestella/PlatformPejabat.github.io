6da9501e4d51c5e60782e7cc3f3ff6d4c535afba
bernadettechrestella <bernadettechrestella@gmail.com>
2021-09-10 09:07:38 +0900

    sprint 1-2

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
